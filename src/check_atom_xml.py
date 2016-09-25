8953374e6fc0cc778c32c5341d8034bdc01a0220
Steve Palmer <steve@srpalmer.me.uk>
2016-09-25 12:26:02 +0100

    WIP: fails
    When the configutation file includes the namespace declaration then the
    xpath expressions don't find anything. When the configuration file does
    not include the namespace declarions then it does not pass the RelaxNG
    validation check!

--- src/check_atom_xml.py
+++ src/check_atom_xml.py
@@ -3,6 +3,7 @@
 Tests an atomrc.xml file for errors.
 '''
 
+import logging.config
 import argparse
 import sys
 import pathlib
@@ -13,108 +14,122 @@ import os
 
 __version__ = '0.1'
 
+logging.config.fileConfig(r'logging.conf')
+LOG = logging.getLogger() if __name__ == '__main__' else logging.getLogger(__name__)
 
 def check(filename):
+    LOG.debug("... argument checking")
     if not isinstance(filename, str):
+        LOG.critcal("Invalid filename: %s", repr(filename))
         raise TypeError
 
     path = pathlib.Path(filename)
 
+    LOG.info("Filename Checking")
     if not path.exists():
-        print("Filename does not exist")
-        sys.exit(1)
+        LOG.critical("Filename %s does not exist", repr(filename))
+        return False
 
     if not path.is_file():
-        print("Filename is not a file")
+        LOG.critical("Filename %s is not a file", repr(filename))
         sys.exit(1)
 
     try:
         inf = path.open()
     except:
-        print("Can't open file")
-        sys.exit(1)
+        LOG.critical("Can't open file %s", repr(filename))
+        return False
 
-    if 1:
+    LOG.debug("... reading RelaxNG schema")
+    try:
         RNGparser = etree.XMLParser()
-        RNGdoc = etree.parse('atom.rng', RNGparser)
-        RNGrules = etree.RelaxNG(RNGdoc)
-    else:
-        print("Can't validate configuration XML")
-        RNGrules = None
+        RNG_DOM = etree.parse('atom.rng', RNGparser)
+        RNGschema = etree.RelaxNG(RNG_DOM)
+    except:
+        LOG.warning("Can't validate file %s", repr(filename))
+        RNGschema = None
 
+    LOG.debug("... reading DOM file")
     XMLparser = etree.XMLParser()
-
     try:
         atomrc = etree.parse(inf, XMLparser)
     except:
-        print("Can't parse file")
-        print("\n".join(XMLparser.error_log))
-        sys.exit(1)
+        LOG.crtical("Can't parse file %s:\n%s", repr(filename), "\n".join(XMLparser.error_log))
+        return False
 
-    if RNGrules:
-        if not RNGrules.validate(atomrc):
-            print("Invalid configuration XML")
+    if RNGschema:
+        LOG.info("Validating file %s against RelaxNG schema", repr(filename))
+        if not RNGschema.validate(atomrc):
+            LOG.error("Invalid configuration XML in file %s", repr(filename))
         
+    LOG.debug("... building memory model")
     devices = [None]
     memorymap = array.array('H', [0]*65536)
 
     for device in atomrc.xpath("/atom/memorymap/*"):
+        LOG.debug("... processing: %s" % etree.tostring(device))
         devices.append(device)
         device_index = devices.index(device)
-        device_base = int(device.xpath("base")[0].text, base=16)
-        if device.tag == 'rom':
+        if device.tag in ('ram', 'ppia'):
+            device_base = int(device.xpath("base")[0].text, base=16)
+            device_size = int(device.xpath("size")[0].text, base=16)
+        elif device.tag == 'rom':
+            device_base = int(device.xpath("base")[0].text, base=16)
             device_filename = device.xpath("filename")[0].text
             if not os.path.exists(device_filename):
-                print("ROM references a missing file:", device_filename)
+                LOG.error("Device base %04X: ROM references a missing file: %s", device_base, repr(device_filename))
             elif not os.path.isfile(device_filename):
-                print("ROM references an inappropriate file:", device_filename)
+                LOG.error("Device base %04X: ROM references an inappropriate file: %s", device_base, repr(device_filename))
             else:
                 file_size = os.stat(device_filename).st_size
                 device_size = device.xpath("size")
                 device_size = file_size if len(device_size) == 0 else int(device_size[0].text, base=16)
                 if file_size < device_size:
-                    print("ROM references too small a file:", device_filename)
+                    LOG.error("Device base %04X: ROM references too small a file: %s", device_base, repr(device_filename))
         else:
-            device_size = int(device.xpath("size")[0].text, base=16)
+            LOG.warning("Unknown type of memory mapped device: %s", repr(device.tag))
         if device_base + device_size > 65536:
-            print("Out of range Device address")
+            LOG.error("Device base %04X: Overflows memory space", device_base)
             device_size = 65536 - device_base
         else:
             for addr in range(device_base, device_base+device_size):
                 if memorymap[addr]:
-                    print("Overlapping Device address:", hex(addr))
+                    LOG.error("Overlapping Devices at address %04X", addr)
+                    break
                 else:
                     memorymap[addr] = device_index
 
-    # Check out the description of Block0 RAM
-
+    LOG.info('Checking Block0 RAM')
     for addr in range(0, 0x0400):
         if memorymap[addr] == 0 or devices[memorymap[addr]].tag != 'ram':
-            print("Missing RAM in block 0 space at address:", hex(addr))
+            LOG.error("Missing RAM in block 0 space at address %04X", addr)
+            break
     
-    # Check out the description of the Video RAM
-
+    LOG.info('Checking Video RAM')
     for addr in range(0x8000, 0x8400):
         if memorymap[addr] == 0 or devices[memorymap[addr]].tag != 'ram':
-            print("Missing RAM in video space at address:", hex(addr))
+            LOG.error("Missing RAM in video space at address %04X", addr)
+            break
     
-    # Check out the description of the PPIA
-
+    LOG.info('Checking PPIA')
     for addr in range(0xB000, 0xB004):
         if memorymap[addr] == 0 or devices[memorymap[addr]].tag != 'ppia':
-            print("Missing PPIA at address:", hex(addr))
+            LOG.error("Missing PPIA at address %04X", addr)
+            break
     
-    # Check out the description of the Kernel
-
+    LOG.info('Checking Kernel ROM')
     for addr in range(0xF000, 0x10000):
         if memorymap[addr] == 0 or devices[memorymap[addr]].tag != 'rom':
-            print("Missing ROM in kernel space at address:", hex(addr))
+            LOG.error("Missing kernel ROM at address %04X", addr)
+            break
 
-    # Check out the description of the Interpreter
-            
+    LOG.info('Checking Interpreter ROM')
     for addr in range(0xC000, 0xD000):
         if memorymap[addr] == 0 or devices[memorymap[addr]].tag != 'rom':
-            print("Missing ROM in Interpret space at address:", hex(addr))
+            LOG.error("Missing Interpreter ROM at address %04X", addr)
+            break
+
+    return True
 
 def main():
     parser = argparse.ArgumentParser(description="Check the content of an atom XML configuration file")
