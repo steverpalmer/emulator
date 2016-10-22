45bcf433c5e55a59a2da4ffb0359df60d14ca800
Steve Palmer <steve@srpalmer.me.uk>
2016-10-22 08:00:09 +0100

    get Scons to use all available processors

--- site_scons/site_init.py
+++ site_scons/site_init.py
@@ -1,14 +1,9 @@
 import subprocess
 
-def add_library(env, lib):
-    def convert(cmd, prefix):
-        return [ (opt[2:] if opt.startswith(prefix) else opt) for opt in cmd.split() ]
-    env.AppendUnique( CFLAGS    = subprocess.check_output(['pkg-config', '--cflags-only-other', lib]).split()
-                    , CPPPATH   = convert(subprocess.check_output(['pkg-config', '--cflags-only-I', lib]), '-I')
-                    , LINKFLAGS = subprocess.check_output(['pkg-config', '--libs-only-other', lib]).split()
-                    , LIBPATH   = convert(subprocess.check_output(['pkg-config', '--libs-only-L', lib]), '-L')
-                    , LIBS      = convert(subprocess.check_output(['pkg-config', '--libs-only-l', lib]), '-l')
-                    )
+try:
+    SetOption('num_jobs', int(subprocess.check_output('nproc')))
+except:
+    pass
 
 uxf2png = Builder(action='umlet -action=convert -format=png -filename=$SOURCE',
                   suffix='uxf.png', src_suffix='uxf')
