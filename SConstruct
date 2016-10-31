78ec4db0eddaebfd2b50a7f4c531759da23602ad
Steve Palmer <steve@srpalmer.me.uk>
2016-10-31 16:43:30 +0000

    Simplified and extended the builders

--- SConstruct
+++ SConstruct
@@ -2,9 +2,6 @@ Help("""
 Builder for my Acorn Atom Emulation
 """)
 
-common_env = Environment()
-Export('common_env')
-
 SConscript([ 'src/SConscript'
            , 'design/SConscript'
            ])
--- site_scons/site_init.py
+++ site_scons/site_init.py
@@ -5,10 +5,18 @@ try:
 except:
     pass
 
-uxf2png = Builder(action='umlet -action=convert -format=png -filename=$SOURCE',
-                  suffix='uxf.png', src_suffix='uxf')
+common_env = Environment()
 
-rng2rnc = Builder(action='trang -I rng -O rnc $SOURCE $TARGET',
-                  suffix='rnc', src_suffix='rng')
-rng2xsd = Builder(action='trang -I rng -O xsd $SOURCE $TARGET',
-                  suffix='xsd', src_suffix='rng')
+common_env['BUILDERS']['uxf2png'] = Builder(action='umlet -action=convert -format=png -filename=$SOURCE',
+                                            suffix='uxf.png', src_suffix='uxf')
+
+common_env['BUILDERS']['rng2rnc'] = Builder(action='trang -I rng -O rnc $SOURCE $TARGET 2>/dev/null',
+                                            suffix='rnc', src_suffix='rng')
+common_env['BUILDERS']['rnc2rng'] = Builder(action='trang -I rnc -O rng $SOURCE $TARGET 2>/dev/null',
+                                            suffix='rng', src_suffix='rnc')
+common_env['BUILDERS']['rng2xsd'] = Builder(action='trang -I rng -O xsd $SOURCE $TARGET 2>/dev/null',
+                                            suffix='xsd', src_suffix='rng')
+common_env['BUILDERS']['rnc2xsd'] = Builder(action='trang -I rnc -O xsd $SOURCE $TARGET 2>/dev/null',
+                                            suffix='xsd', src_suffix='rnc')
+
+Export('common_env')
