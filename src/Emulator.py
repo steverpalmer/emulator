ffa4c3b57991eb15fa2abd62c626702e4c55efca
Steve Palmer <steve@srpalmer.me.uk>
2016-12-03 15:29:37 +0000

    Lost the connection logging because it was being done in a different thread - now recovered.

--- src/Emulator.py
+++ src/Emulator.py
@@ -17,7 +17,11 @@ from robot.utils import ConnectionCache
 from robot.utils import asserts
 
 # http://stackoverflow.com/questions/6893968/how-to-get-the-return-value-from-a-thread-in-python
-class ThreadWithReturnValue(Thread):
+class ThreadWithReturnAndLog(Thread):
+    '''
+    The Library reference states "...only override the __init__() and run() methods of this class."
+    However, the trick below seems to work ...
+    '''
     def __init__(self, group=None, target=None, name=None, args=(), kwargs={}):
         Thread.__init__(self, group, target, name, args, kwargs)
         self._return = None
@@ -25,7 +29,7 @@ class ThreadWithReturnValue(Thread):
         if self._Thread__target is not None:
             self._return = self._Thread__target(*self._Thread__args, **self._Thread__kwargs)
     def join(self, timeout=None):
-        super(ThreadWithReturnValue, self).join(timeout)
+        super(ThreadWithReturnAndLog, self).join(timeout)
         return self._return
 
 __metaclass__ = type
@@ -39,27 +43,38 @@ class Emulator:
                  executable="./emulator",
                  config_file="testrc.xml",
                  prompt=">",
-                 default_log_level='INFO',
+                 log_level='INFO',
                  timeout=1.0):
         self._executable = executable
         self._config_file = config_file
         self._default_prompt = prompt
-        self._default_log_level = default_log_level
+        self._log_level = log_level
         self._timeout = timeout
         self._cache = ConnectionCache()
         self._thread = None
 
-    def _log(self, msg, level=None):
+    def _log(self, msg, log_level=None):
         msg = msg.strip()
         if (msg):
-            logger.write(msg, level or self._default_log_level)
+            logger.write(msg, log_level or self._log_level)
 
-    def _execute(self, thread, timeout=None):
+    def log_executable_name(self, log_level=None):
+        self._log(self._executable, log_level)
+    
+    def log_configuration_file(self, log_level=None):
+        self._log(self._config_file, log_level)
+
+    def _execute(self, thread, log_level=None, timeout=None):
+        connection = self._cache.get_connection() 
+        connection.clear_log()
         thread.start()
         result = thread.join(timeout or self._timeout)
+        self._log(connection.log, log_level)
         if thread.is_alive():
             asserts.fail("Timedout")
-            self._cache.get_connection().reset(log_level)
+            connection.clear_log()
+            connection.reset()
+            self._log(connection.log, log_level)
         return result
 
     def open_connection(self, alias=None,
@@ -71,8 +86,8 @@ class Emulator:
         connection = EmulatorConnection(executable or self._executable,
                                         config_file or self._config_file,
                                         prompt or self._default_prompt,
-                                        log_level or self._default_log_level,
                                         timeout or self._timeout)
+        self._log(connection.log, log_level)
         result = self._cache.register(connection, alias)
         self._log("Active Connection: %s" % repr(result))
         return result
@@ -98,77 +113,70 @@ class Emulator:
 
     def close_connection(self, log_level=None, timeout=None):
         """Closes the current connection"""
-        self._execute(Thread(target=self._cache.get_connection().close_connection,
-                             args=(log_level,),
-                             name='close_connection'),
-                      timeout or self._timeout * 3)
+        self._execute(ThreadWithReturnAndLog(target=self._cache.get_connection().close_connection,
+                                             name='close_connection'),
+                      log_level, timeout or self._timeout * 3)
         self.current_index = None
         self._log("Active Connection: %s" % repr(self._cache.current_index), log_level)
 
     def set_prompt(self, prompt=None):
         self._cache.get_connection().prompt = prompt or self._default_prompt
 
-    def set_log_level(self, level=None):
-        self._cache.get_connection().log_level = level or self._default_log_level
+    def set_log_level(self, log_level=None):
+        self._cache.get_connection().log_level = log_level or self._log_level
 
     def write_bare(self, text, log_level=None, timeout=None):
-        self._execute(Thread(target=self._cache.get_connection().write_bare,
-                                            args=(text, log_level),
-                                            name='write_bare'),
-                      timeout)
+        self._execute(ThreadWithReturnAndLog(target=self._cache.get_connection().write_bare,
+                                             args=(text,),
+                                             name='write_bare'),
+                      log_level, timeout)
 
     def write(self, text, log_level=None, timeout=None):
-        self._execute(Thread(target=self._cache.get_connection().write,
-                                            args=(text, log_level),
-                                            name='write'),
-                      timeout)
+        self._execute(ThreadWithReturnAndLog(target=self._cache.get_connection().write,
+                                             args=(text,),
+                                             name='write'),
+                      log_level, timeout)
 
     def read(self, log_level=None, timeout=None):
-        result = self._execute(ThreadWithReturnValue(target=self._cache.get_connection().read,
-                                                     args=(log_level,),
+        result = self._execute(ThreadWithReturnAndLog(target=self._cache.get_connection().read,
                                                      name='read'),
-                               timeout)
+                               log_level, timeout)
         return result
 
     def read_line(self, log_level=None, timeout=None):
-        result = self._execute(ThreadWithReturnValue(target=self._cache.get_connection().read_line,
-                                                     args=(log_level,),
+        result = self._execute(ThreadWithReturnAndLog(target=self._cache.get_connection().read_line,
                                                      name='read_line'),
-                               timeout)
+                               log_level, timeout)
         return result
 
     def read_until(self, expect, log_level=None, timeout=None):
-        result = self._execute(ThreadWithReturnValue(target=self._cache.get_connection().read_until,
-                                                     args=(expect, log_level),
+        result = self._execute(ThreadWithReturnAndLog(target=self._cache.get_connection().read_until,
+                                                     args=(expect,),
                                                      name='read_until'),
-                               timeout)
+                               log_level, timeout)
         return result
 
     def read_until_prompt(self, log_level=None, timeout=None):
-        result = self._execute(ThreadWithReturnValue(target=self._cache.get_connection().read_until_prompt,
-                                                     args=(log_level,),
+        result = self._execute(ThreadWithReturnAndLog(target=self._cache.get_connection().read_until_prompt,
                                                      name='read_until_prompt'),
-                               timeout)
+                               log_level, timeout)
         return result
 
     def reset(self, log_level=None, timeout=None):
-        self._execute(Thread(target=self._cache.get_connection().reset,
-                             args=(log_level,),
-                             name='reset'),
-                      timeout)
+        self._execute(ThreadWithReturnAndLog(target=self._cache.get_connection().reset,
+                                             name='reset'),
+                      log_level, timeout)
 
 class EmulatorConnection:
     """Handles the state associated with a specific emulator run
     """
 
-    def __init__(self, executable, config_file, prompt, log_level, timeout):
+    def __init__(self, executable, config_file, prompt, timeout):
         self._prompt = prompt
-        self._log_level = log_level
         self._timeout = timeout
-        self._popen = subprocess.Popen([executable,'-f',config_file],
-                                       bufsize=0,
-                                       stdin=subprocess.PIPE,
-                                       stdout=subprocess.PIPE)
+        command = [executable,'-f',config_file]
+        self._log = "Exec:%s\n" % (" ".join(command))
+        self._popen = subprocess.Popen(command, bufsize=0, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         self._popen.poll()
         self._pid = self._popen.pid
         self._returncode = self._popen.returncode
@@ -180,22 +188,17 @@ class EmulatorConnection:
     @prompt.setter
     def prompt(self, prompt): self._prompt = prompt
 
+    def clear_log(self):
+        self._log=""
+
     @property
-    def log_level(self): return self._log_level
+    def log(self): return self._log
 
-    @log_level.setter
-    def log_level(self, log_level): self._log_level = log_level
+    def _read_log(self, text):
+        self._log += "Rx:%s\n" % repr(text)
 
-    def _log(self, msg, level=None):
-        msg = msg.strip()
-        if (msg):
-            logger.write(msg, level or self._log_level)
-
-    def _read_log(self, text, log_level=None):
-        self._log("Rx:" + repr(text), log_level)
-
-    def _write_log(self, text, log_level=None):
-        self._log("Tx:" + repr(text), log_level)
+    def _write_log(self, text):
+        self._log += "Tx:%s\n" % repr(text)
 
     def close(self):
         if self._popen:
@@ -213,39 +216,39 @@ class EmulatorConnection:
                     self._returncode = self._popen.returncode
                 except subprocess.TimeoutExpired:
                     pass
-            self._log("Return Code:" + repr(self._returncode))
+            self._log += "Return Code:%s\n" % repr(self._returncode)
 
-    def close_connection(self, log_level=None):
+    def close_connection(self):
         if self._popen:
             self._popen.stdin.close()
-            result = self.read(log_level)
+            result = self.read()
             self._popen.poll()
             self._returncode = self._popen.returncode
-            self._log("Return Code:" + repr(self._returncode), log_level)
+            self._log += "Return Code:%s\n" % repr(self._returncode)
             self._popen = None
         if self._popen:
             self.close()
         self._popen = None
         return result
 
-    def write_bare(self, text, log_level=None):
+    def write_bare(self, text):
         self._write_log(text)
         if (self._popen):
             self._popen.stdin.write(text)
 
-    def write(self, text, log_level=None):
+    def write(self, text):
         text += '\n'
-        self.write_bare(text, log_level)
-        self.read_until(text, log_level)
+        self.write_bare(text)
+        self.read_until(text)
 
-    def read(self, log_level=None):
+    def read(self):
         result = self._popen.stdout.read() if self._popen else ''
-        self._read_log(result, log_level)
+        self._read_log(result)
         return result
 
-    def read_line(self, log_level=None):
+    def read_line(self):
         result = self._popen.stdout.readline() if self._popen else ''
-        self._read_log(result, log_level)
+        self._read_log(result)
         return result
 
     def _read_until(self, expected):
@@ -254,20 +257,20 @@ class EmulatorConnection:
             result += self._popen.stdout.read(1)
         return result
 
-    def read_until(self, expected, log_level=None):
+    def read_until(self, expected):
         result = self._read_until(expected)
-        self._read_log(result, log_level)
+        self._read_log(result)
         result = result[:-len(expected)]
         return result
 
-    def read_until_prompt(self, log_level=None):
+    def read_until_prompt(self):
         result = self._read_until(self._prompt)
-        self._read_log(result, log_level)
+        self._read_log(result)
         result = result[:-len(self._prompt)]
         return result
 
-    def reset(self, log_level=None):
+    def reset(self):
         if self._popen:
             self._popen.send_signal(signal.SIGUSR1)
             # self._popen.stdin.write(" ")  # to unblock the processor
-            self._log("Connection Reset", log_level)
+            self._log += "Connection Reset\n"
--- src/test.robot
+++ src/test.robot
@@ -7,6 +7,11 @@ ${BOOT MESSAGE}         *ACORN ATOM*
 ${TEST MESSAGE}         HELLO WORLD
 
 *** Test Cases ***
+Configuration Report
+      [Documentation]   Records some configuration data
+      Log Executable Name
+      Log Configuration File
+
 Make And Close A Connection
       [Documentation]   makes and closes a connection to an emulator
       [Tags]            smoke
@@ -39,12 +44,20 @@ Reset Test
       Should Match      ${result}   ${BOOT MESSAGE}
       Close Connection
 
+# Load Test
+#       [Documentation]   Try and load a file
+#       Start Emulator
+#       Execute           LOAD "ATAP4.1.BAS"
+#       ${result} =       Execute     RUN
+#       Should Be Equal   ${result}   A PROGRAM!
+#       Close Connection
+
 *** Keywords ***
 Start Emulator
       [Documentation]   Make the connection and check it is operating
       Open Connection
       ${bootmessage} =  Read Until Prompt
-      Should Match      ${bootmessage}     *ACORN ATOM*
+      Should Match      ${bootmessage}     ${BOOT MESSAGE}
 
 Execute
       [Arguments]       ${command}
